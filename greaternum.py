b7e8c72516c41524b744735751de32917a9573ca
Maahina <kambojmaahina@gmail.com>
2026-06-28 12:37:19 +0530

    which one is greater

---
+++ greaternum.py
@@ -0,0 +1,7 @@
+'''enter 2 num and print which one is greater'''
+a=int(input("enter 1st num"))
+b=int(input("enter 2nd num"))
+if b>a:
+    print(f"{b} is greater than {a}")
+else:
+    print(f"{a} is greater than {b}")
